ccbe8e4954822cea80e36f4809c44f6ea56a6204
jarednrice <jarednrice@gmail.com>
2020-01-16 16:41:34 -0500

    added function to check existence, haven't filled it out yet

--- path_res.c
+++ path_res.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+int exists(const char *fname){
+  if(access(fname, F_OK) != -1){
+    // file exists
+  }else {
+    // file doesn't exist
+  }
+}
 int main(){
   int size = 4;
   char *PATH_INPUT = (char *)malloc(sizeof(char)*size);
@@ -9,9 +18,17 @@ int main(){
   printf("%s\n", getenv("PATH"));
 
   char * token = strtok(getenv("PATH"), ":");
+  char * temp = (char *)malloc(sizeof(char)*size);
   // loop through to get all paths
   while(token != NULL){
     printf( " %s\n", token );
+    //path concatenated with provided pathname
+    strcpy(temp, token);
+    strcat(temp, "/");
+    strcat(temp, PATH_INPUT);
+    printf(" %s\n", temp);
+    exists(temp);
+    strcpy(temp, "");
     token = strtok(NULL, ":");
   }
 return 0;
