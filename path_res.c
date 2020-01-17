2adc849372b09e38f16ee0962ad3b30c11b521b8
jarednrice <jarednrice@gmail.com>
2020-01-16 22:06:18 -0500

    files can be checked for existence and regularity within /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin

--- path_res.c
+++ path_res.c
@@ -2,14 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-int exists(const char *fname){
+//function to check existence of file
+bool exists(const char *fname){
   if(access(fname, F_OK) != -1){
-    // file exists
-  }else {
-    // file doesn't exist
+    printf("%s\n", "file exists");
+  }
+  else {
+    return false;
   }
 }
+
+//function to check if file is regular
+bool check_regular(const char *fname){
+    struct stat file_stat;
+    stat(fname, &file_stat);
+    return S_ISREG(file_stat.st_mode);
+}
+
 int main(){
   int size = 4;
   char *PATH_INPUT = (char *)malloc(sizeof(char)*size);
@@ -18,8 +31,11 @@ int main(){
   printf("%s\n", getenv("PATH"));
 
   char * token = strtok(getenv("PATH"), ":");
+  //string for concatonated path names
   char * temp = (char *)malloc(sizeof(char)*size);
-  // loop through to get all paths
+  //loop through to get all paths
+  //if flag is still false, file is not executable
+  bool file_flag = false;
   while(token != NULL){
     printf( " %s\n", token );
     //path concatenated with provided pathname
@@ -27,9 +43,17 @@ int main(){
     strcat(temp, "/");
     strcat(temp, PATH_INPUT);
     printf(" %s\n", temp);
-    exists(temp);
+    if(exists(temp) && check_regular(temp)){
+      strcpy(temp, "");
+      file_flag = true;
+      break;
+    }
     strcpy(temp, "");
     token = strtok(NULL, ":");
   }
+
+  if(!file_flag)
+    printf("%s\n", "File does not exist or is not regular");
+
 return 0;
 }
