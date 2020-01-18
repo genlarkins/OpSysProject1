dbc4cfc0e087676205f2a3a1c8afb4ad52f8d7b0
jarednrice <jarednrice@gmail.com>
2020-01-17 19:13:54 -0500

    can execute single command no parameters

--- path_res.c
+++ path_res.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/stat.h>
 
 //function to check existence of file
@@ -18,9 +19,29 @@ bool exists(const char *fname){
 
 //function to check if file is regular
 bool check_regular(const char *fname){
-    struct stat file_stat;
-    stat(fname, &file_stat);
-    return S_ISREG(file_stat.st_mode);
+  struct stat file_stat;
+  stat(fname, &file_stat);
+  return S_ISREG(file_stat.st_mode);
+}
+
+//function to execute commands
+void my_execute(char **cmd) {
+  int status;
+  pid_t pid = fork();
+  if (pid == -1) {
+    //Error
+    exit(1);
+  }
+  else if (pid == 0) {
+    //Child
+    execv(cmd[0], cmd);
+    printf("Problem executing %s\n", cmd[0]);
+    exit(1);
+  }
+  else {
+    //Parent
+    waitpid(pid, &status, 0);
+  }
 }
 
 int main(){
@@ -28,32 +49,38 @@ int main(){
   char *PATH_INPUT = (char *)malloc(sizeof(char)*size);
 
   scanf("%s", PATH_INPUT);
-  printf("%s\n", getenv("PATH"));
+  //printf("%s\n", getenv("PATH"));
 
   char * token = strtok(getenv("PATH"), ":");
   //string for concatonated path names
-  char * temp = (char *)malloc(sizeof(char)*size);
+  char * concat = (char *)malloc(sizeof(char)*size);
+
+  char ** cmd = (char **)malloc(sizeof(char)*size);
   //loop through to get all paths
   //if flag is still false, file is not executable
   bool file_flag = false;
   while(token != NULL){
-    printf( " %s\n", token );
+    //printf( " %s\n", token );
     //path concatenated with provided pathname
-    strcpy(temp, token);
-    strcat(temp, "/");
-    strcat(temp, PATH_INPUT);
-    printf(" %s\n", temp);
-    if(exists(temp) && check_regular(temp)){
-      strcpy(temp, "");
+    strcpy(concat, token);
+    strcat(concat, "/");
+    strcat(concat, PATH_INPUT);
+    //printf(" %s\n", concat);
+    if(exists(concat) && check_regular(concat)){
+      //strcpy(concat, "");
       file_flag = true;
       break;
     }
-    strcpy(temp, "");
+    strcpy(concat, "");
     token = strtok(NULL, ":");
   }
 
   if(!file_flag)
     printf("%s\n", "File does not exist or is not regular");
+  else {
+    cmd[0] = concat;
+    my_execute(cmd);
+  }
 
 return 0;
 }
