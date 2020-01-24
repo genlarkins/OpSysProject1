00d52eb39e4609aac4992a17bb729244e0ccb638
Madison Lidian Vandersluis <vandersl@linprog4.cs.fsu.edu>
2020-01-24 10:54:10 -0500

    Completed implementation of part 3: prompt

--- madis_part.c
+++ madis_part.c
@@ -6,21 +6,22 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 char* prompt();
 void bgProcess(char* data);
 void checkBGP();
-void ioRedirection(char* cmd, char* file, int i_o);
-void ioRedirection(char* cmd, char* file_in, char* file_out);
+void ioRedir1(char* cmd, char* file, int i_o);
+void ioRedir2(char* cmd, char* file_in, char* file_out);
 
 int main(){
 	
 	//prompt user
 	char* userInput= "nothing";
-	while(strcmp(userinput, "exit(1)") != 0){
+	while(strcmp(userInput, "exit(1)") != 0){
 		userInput = prompt();
-		printf("Got the input: %s", userinput);
+		printf("Got the input: %s\n", userInput);
 		checkBGP();
 		//parse input
 	}
@@ -29,52 +30,76 @@ int main(){
 }
 
 char* prompt(){
-	char* input;
+	char* input = calloc(100, sizeof(char));
 	char* user = getenv("USER");
 	char* machine = getenv("MACHINE");
 	char* pwd = getenv("PWD");
-	printf("%s@%s: %s >", user, machine, pwd);
+	printf("%s@%s: %s>", user, machine, pwd);
 	//get the command a user inputs
 	fgets(input, 50, stdin);
+	strtok(input, "\n");
 	return input;
 }
 
-void bgProcess(char* data){}
-void checkBGP(){}
-void ioRedirection(char* cmd, char* file, int i_o){
+void bgProcess(char* data){
+	
+}
+void checkBGP(){
+	printf("Checking if BG process!\n");
+}
+void ioRedir1(char* cmd, char* file, int i_o){
 	int fd, buff;
 	char* fileData;
 	if(i_o==0){ //output
 		//redirect cmd result to file
+		fd = open(file, O_CREAT | O_WRONLY);
+		if(fd==-1){ //could not create
+			printf("Error creating file: %s", file);
+		}
+		else {
+			if(fork() == 0){
+				close(STDOUT_FILENO);
+				dup(fd);
+				close(fd);
+				printf("Output Child done!");
+			}
+			else {
+				close(fd);
+				printf("Output parent done!");
+			}
+		}
 	}
 	else if(i_o==1){ //input
 		//check if file exists
 		//error if it doesnt else
 		//execute cmd with file as input
-		fd = open(file);
+		fd = open(file, O_RDONLY);
 		if(fd == -1){ //means error opening file
 			printf("Error opening file: %s", file);
 		}
 		else{
-			if(forked() == 0){
+			if(fork() == 0){
 				//child
 				close(STDIN_FILENO);
 				dup(fd);
 				close(fd);
 				//execute process bscially it will read in from the file which is now "stdin"
 				//call execution using fileData as input
+				printf("Input Child done!");
 			}
 			else {
 				//parent
 				close(fd);
+				printf("input parent done!");
 			}
 		}
 	} else{
 		//error
+		printf("Error: bad syntax");
 	}
 	
 }
-void ioRedirection(char* cmd, char* file_in, char* file_out){
+void ioRedir2(char* cmd, char* file_in, char* file_out){
 	//check if file_in exists error if it dont
 	//cmd takes filein as input and saves result in fileout
 }
