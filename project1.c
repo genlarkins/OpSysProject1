d04ce871e3411e0b9006780948116d8cc1a92631
jarednrice <jarednrice@gmail.com>
2020-01-24 20:41:21 -0500

    added in parser

--- project1.c
+++ project1.c
@@ -8,6 +8,16 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
+typedef struct {
+	char** tokens;
+	int numTokens;
+} instruction;
+
+void addToken(instruction* instr_ptr, char* tok);
+void printTokens(instruction* instr_ptr);
+void clearInstruction(instruction* instr_ptr);
+void addNull(instruction* instr_ptr);
+
 char* prompt();
 
 //function to check existence of file
@@ -17,34 +27,81 @@ bool exists(const char *fname);
 bool check_regular(const char *fname);
 
 //function to execute commands
-void my_execute(char **cmd); 
+void my_execute(char **cmd);
 
 int main(){
-	
-	//prompt user
+
 	int size = 4;
 	char *userInput = (char *)malloc(sizeof(char)*size);
-	char * token = strtok(getenv("PATH"), ":");
+	int counter=0;
+
+	char* token = NULL;
+	char* temp = NULL;
+
+	instruction instr;
+	instr.tokens = NULL;
+	instr.numTokens = 0;
+
+	//$PATH
+	char * path = strtok(getenv("PATH"), ":");
 	//string for concatonated path names
 	char * concat = (char *)malloc(sizeof(char)*size);
 	//string containing command + args
 	char ** cmd = (char **)malloc(sizeof(char)*size);
-	int counter=0;
+
+	int loopcount = 0;
+	int firstIns = 0;
+
 	while(strcmp(userInput, "exit") != 0){
+		//PROMPT USER
 		strcpy(userInput, "");
 		userInput = prompt();
-		printf("Got the input: %s\n", userInput);
-		//parse input
-		char* test= malloc(sizeof(char));
-		//execute counter++ for every command executed
-		
+		//PARSE INPUT
+		do {
+			scanf("%ms",&token);
+			temp = (char*) calloc(100, (strlen((token)+1) * sizeof(char)));
+			int i;
+			int start = 0;
+			for (i=0; i<strlen(token); i++) {
+				if(token[i] == '|' || token[i] == '>' || token[i] == '<'
+				   || token[i] == '&') {
+					if(i-start > 0){
+						memcpy(temp, token + start, i-start);
+						temp[i-start] = '\0';
+						addToken(&instr, temp);
+					}
+					char specialChar[2];
+					specialChar[0] = token[i];
+					specialChar[1] = '\0';
+					addToken(&instr,specialChar);
+
+					start = i+1;
+				}
+			}
+			if (start < strlen(token)) {
+				memcpy(temp, token + start, strlen(token) - start);
+				temp[i-start] = '\0';
+				addToken(&instr, temp);
+			}
+			//free and reset variables
+			free(token);
+			free(temp);
+			token = NULL;
+			temp = NULL;
+		} while ('\n' != getchar());
+
+		addNull(&instr);
+		printTokens(&instr);
+
+		//EXECUTE COMMANDS
+		clearInstruction(&instr);
 		//loop through to get all paths
 		//if flag is still false, file is not executable
 		bool file_flag = false;
-		while(token != NULL){
+		while(path != NULL){
 			//printf( " %s\n", token );
 			//path concatenated with provided pathname
-			strcpy(concat, token);
+			strcpy(concat, path);
 			strcat(concat, "/");
 			strcat(concat, userInput);
 			//printf(" %s\n", concat);
@@ -52,21 +109,82 @@ int main(){
 				//strcpy(concat, "");
 				file_flag = true;
 				break;
-			}  
+			}
 			strcpy(concat, "");
-			token = strtok(NULL, ":");
-		} 
+			path = strtok(NULL, ":");
+		}
 
     if(!file_flag)
       printf("%s\n", "File does not exist or is not regular");
     else {
       cmd[0] = concat;
+			//cmd[1] = "-l";
       my_execute(cmd);
     }
     //strcpy(userInput, "");
+	}
+	free(userInput);
+	printf("Exiting now!\n");
+	return 0;
 }
-return 0;		
+
+void addToken(instruction* instr_ptr, char* tok)
+{
+	//extend token array to accomodate an additional token
+	if (instr_ptr->numTokens == 0)
+		instr_ptr->tokens = (char**) calloc(100,sizeof(char*));
+	else
+		instr_ptr->tokens = (char**) realloc(instr_ptr->tokens, (instr_ptr->numTokens+1) * sizeof(char*));
+
+	//allocate char array for new token in new slot
+	instr_ptr->tokens[instr_ptr->numTokens] = (char *)malloc((strlen(tok)+1) * sizeof(char));
+	strcpy(instr_ptr->tokens[instr_ptr->numTokens], tok);
+
+	instr_ptr->numTokens++;
 }
+
+void addNull(instruction* instr_ptr)
+{
+	//extend token array to accomodate an additional token
+	if (instr_ptr->numTokens == 0)
+		instr_ptr->tokens = (char**)calloc(100, sizeof(char*));
+	else
+		instr_ptr->tokens = (char**)realloc(instr_ptr->tokens, (instr_ptr->numTokens+1) * sizeof(char*));
+
+	instr_ptr->tokens[instr_ptr->numTokens] = (char*) NULL;
+	instr_ptr->numTokens++;
+}
+
+void printTokens(instruction* instr_ptr)
+{
+	int i;
+	printf("Tokens:\n");
+	for (i = 0; i < instr_ptr->numTokens; i++) {
+		if ((instr_ptr->tokens)[i] != NULL)
+			printf("%s\n", (instr_ptr->tokens)[i]);
+	}
+}
+
+void clearInstruction(instruction* instr_ptr)
+{
+	int i;
+	for (i = 0; i < instr_ptr->numTokens; i++)
+		free(instr_ptr->tokens[i]);
+
+	free(instr_ptr->tokens);
+
+	instr_ptr->tokens = NULL;
+	instr_ptr->numTokens = 0;
+}
+
+/*void prompt(){
+	char* user = getenv("USER");
+	char* machine = getenv("MACHINE");
+	char* pwd = getenv("PWD");
+	printf("%s@%s: %s>", user, machine, pwd);
+	//get the command a user inputs
+}*/
+
 char* prompt(){
 	char* input = malloc(sizeof(char));
 	char* user = getenv("USER");
